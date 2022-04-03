1e642879897c9d22b3a49a2d57b702d3e97ba11a
pvcapuano <pvcapuano@gmail.com>
2022-04-03 11:15:24 -0300

    add headers

--- FinApi/src/index.js
+++ FinApi/src/index.js
@@ -6,13 +6,22 @@ const app = express();
 
 app.use(express.json());
 
-const costumers = [];
+const customers = [];
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" });
+  }
+
   const id = uuidv4();
 
-  costumers.push({
+  customers.push({
     cpf,
     name,
     id,
@@ -20,6 +29,18 @@ app.post("/account", (request, response) => {
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement/", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
